3d92c36631acfb8ecab0944579eaaeb113810d2a
Federico Piccirilli <federico.piccirilli@pt.bosch.com>
2024-11-19 12:33:54 +0000

    fix: repair broken reference, use CatImage instead of Cat

--- Cats/Domain/Network/CatService.swift
+++ Cats/Domain/Network/CatService.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 protocol CatServiceProtocol {
-  func fetchCats(by string: String?) async throws -> Cat
+  func fetchCatImage(by string: String?) async throws -> CatImage
   func getCat(by breed: String) async throws -> CatBreed
   func addVote(vote: Int, id: String) async throws -> DefaultResponse
 }
 
 class CatNetworkService: BaseNetworkService<Router>, CatServiceProtocol {
 
-  func fetchCats(by string: String?) async throws -> Cat {
-    return try await request(Cat.self, router: .getCats)
+  func fetchCatImage(by string: String?) async throws -> CatImage {
+    return try await request(CatImage.self, router: .getCats)
   }
 
   func getCat(by breed: String) async throws -> CatBreed {
