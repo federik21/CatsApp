3d02dba01b3392600b52c5003ccb40612971e272
Federico Piccirilli <federico.piccirilli@pt.bosch.com>
2024-11-19 14:34:19 +0000

    feat: add implementation of cat manager

--- Cats/Manager/CatManager.swift
+++ Cats/Manager/CatManager.swift
@@ -11,8 +11,42 @@ enum Vote {
   case up, down, neutral
 }
 
-protocol CatManager {
-  func getFavourites() -> CatImage
-  func searchByBreed() -> CatImage
-  func vote(breed: String, vote: Vote?)
+protocol CatManagerProtocol {
+  func getFavourites() async
+  func searchByBreed() async
+  func vote(breed: String, vote: Vote) async
+}
+
+class CatManager: CatManagerProtocol {
+
+  var cats = [CatImage]()
+
+  let networkClient: CatNetworkService
+
+  init(networkClient: CatNetworkService) {
+    self.networkClient = networkClient
+  }
+
+  func getFavourites() async {
+    cats = try! await networkClient.fetchCatImages(by: nil)
+  }
+
+  func searchByBreed() async {
+    cats = try! await networkClient.fetchCatImages(by: nil)
+  }
+
+  func vote(breed: String, vote: Vote) async {
+    var voteInt = 0
+    switch vote {
+    case .up:
+      voteInt = 1
+    case .down:
+      voteInt = -1
+    case .neutral:
+      voteInt = 0
+    }
+    _ = try? await networkClient.addVote(vote: voteInt, id: breed)
+  }
+
+
 }
